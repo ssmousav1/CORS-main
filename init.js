03584a9707b0bd3e5780aba3c50540c64f08c184
kal9isa <docalisa_it@yahoo.com>
2021-05-21 03:00:05 -0700

    run commands sync style

--- init.js
+++ init.js
@@ -1,30 +1,39 @@
-const cmd=require('node-cmd');
+const cmd = require("node-cmd");
 
 //*nix supports multiline commands
 
+let commands = [`sudo ./start.sh`];
+// let commands = [`sudo ./start.sh`, `node config.js`, `pm2 start server.js`];
 
+commands.forEach((command) => {
+  let res;
+  try {
+    res = cmd.runSync(command);
+  } catch (error) {
+    console.log(error);
+		break;
+  }
+});
 
-    cmd.run(
-        `sudo ./start.sh
-	ls`,
-        function(err, data, stderr){
-            console.log('examples dir now contains the example file along with : ',data)
-		
-		console.log(stderr,'**********')
-		if(err){
-			console.log(err)
-		
-		}else{
-			console.log('$$$$$$$$')
-			cmd.run(
-                                `node config.js`,
-                        function(err, data, stderr){
-                                console.log('examples dir now contains the example file along with : ',data)
-                                }
-                        );
+// cmd.run(
+//   ,
+//   function (err, data, stderr) {
+//     console.log(
+//       "examples dir now contains the example file along with : ",
+//       data
+//     );
 
-		}
-
-        }
-    );
-
+//     console.log(stderr, "**********");
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("$$$$$$$$");
+//       cmd.run(`node config.js`, function (err, data, stderr) {
+//         console.log(
+//           "examples dir now contains the example file along with : ",
+//           data
+//         );
+//       });
+//     }
+//   }
+// );
