65045de041b2886d5c943ba70de88be630ee4a67
kal9isa <docalisa_it@yahoo.com>
2021-05-22 02:45:20 -0700

    network config test

---
+++ net-conf.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const cmd = require("node-cmd");
+
+function setNetwork() {
+  let contents = `ifconfig eth0 ${process.env.UD_IP} netmask ${process.env.UD_NMASK}\r\n`;
+  fs.appendFileSync("netconfig.sh", contents, { mode: "0o777" });
+  contents = `route add default gw ${process.env.UD_GW}\r\n`;
+  fs.appendFileSync("netconfig.sh", contents, { mode: "0o777" });
+  console.log(process.env.UD_IP);
+  console.log(process.env.UD_NMASK);
+  console.log(process.env.UD_GW);
+  fs.closeSync();
+}
+
+setNetwork();
