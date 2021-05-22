c4278779ce295ce92b64fe6a39fcc05db0191c3c
kal9isa <docalisa_it@yahoo.com>
2021-05-22 03:11:49 -0700

    fd added

--- net-conf.js
+++ net-conf.js
@@ -2,14 +2,15 @@ const fs = require("fs");
 const cmd = require("node-cmd");
 
 function setNetwork() {
+  let fd = fs.openSync("netconfig.sh", "rw", 0o777);
   let contents = `ifconfig eth0 ${process.env.UD_IP} netmask ${process.env.UD_NMASK}\r\n`;
-  fs.appendFileSync("netconfig.sh", contents, { mode: 511 });
+  fs.appendFileSync(fd, contents, { mode: 511 });
   contents = `route add default gw ${process.env.UD_GW}\r\n`;
-  fs.appendFileSync("netconfig.sh", contents, { mode: 511 });
+  fs.appendFileSync(fd, contents, { mode: 511 });
   console.log(process.env.UD_IP);
   console.log(process.env.UD_NMASK);
   console.log(process.env.UD_GW);
-  fs.closeSync();
+  fs.closeSync(fd);
 }
 
 setNetwork();
