5539f28c76de42de5bd02c21f30a5d77def61064
Sobhan Mousavi <sobhanmousavi121122@yahoo.com>
2021-05-22 16:15:29 +0430

    setIP added to netconfig

---
+++ helpers/netConfig.js
@@ -0,0 +1,32 @@
+
+const fs = require("fs");
+const cmd = require("node-cmd");
+const nc = require("network-calculator");
+
+const pathBASE = process.cwd();
+
+const setIP = (params) => {
+  console.log('params >>>>>>>>',params);
+  // let ip = JSON.parse(params.ip);
+  // let subnet = JSON.parse(params.subnet);
+  // // ip addr add 192.168.2.24/24 dev eth0
+  // // ifconfig eth0 192.168.1.5 netmask 255.255.255.0 up
+  // console.log(`ip: ${typeof ip} ${ip}`);
+  // console.log(`sub: ${typeof subnet} ${subnet}`);
+  // try {
+  //   let bitmask = nc(ip, subnet).bitmask;
+  //   let raw = cmd.runSync(`rm ${pathBASE}/commands/set-ip.sh`);
+  //   console.log(raw);
+  //   const command = `ip addr add ${ip}/${bitmask} dev eth0`;
+  //   fs.writeFileSync(`${pathBASE}/commands/set-ip.sh`, command);
+  //   console.log("success creating ip file!");
+  //   raw = cmd.runSync(`${pathBASE}/scripts/config.sh ip`);
+  //   console.log(raw);
+  //   // eventlib.emit("msg:log", { status: 200, msg: "Done configuring IP" });
+  // } catch (error) {
+  //   console.error(error);
+  // }
+}
+
+
+module.exports = { setIP }
