403c3504d7f0930d028592f71528731a8f0bd0ae
kal9isa <docalisa_it@yahoo.com>
2021-05-22 03:04:53 -0700

    fixed permission

--- net-conf.js
+++ net-conf.js
@@ -3,9 +3,9 @@ const cmd = require("node-cmd");
 
 function setNetwork() {
   let contents = `ifconfig eth0 ${process.env.UD_IP} netmask ${process.env.UD_NMASK}\r\n`;
-  fs.appendFileSync("netconfig.sh", contents, { mode: "0o777" });
+  fs.appendFileSync("netconfig.sh", contents, { mode: 511 });
   contents = `route add default gw ${process.env.UD_GW}\r\n`;
-  fs.appendFileSync("netconfig.sh", contents, { mode: "0o777" });
+  fs.appendFileSync("netconfig.sh", contents, { mode: 511 });
   console.log(process.env.UD_IP);
   console.log(process.env.UD_NMASK);
   console.log(process.env.UD_GW);
